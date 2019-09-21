83800c17ff7d99f94d73e2a5650dce5e9dec7d4e
Gitpvd <pvdayanand@gmail.com>
2019-09-21 04:07:00 -0500

    updated

--- UFO-Level 1/static/js/index.js
+++ UFO-Level 1/static/js/index.js
@@ -10,6 +10,8 @@ data.forEach(sitingsReport => {
   });
 });
 
+var sitings = data;
+
 // Select the button
 var button = d3.select("#button");
 
@@ -24,7 +26,37 @@ button.on("click", function() {
 
   console.log(inputValue);
 
-  // Set the span tag in the h1 element to the text
-  // that was entered in the form
+  // Set the span tag in the h1 element to the text that was entered in the form
   d3.select("h1>span").text(inputValue);
+
+  console.log(sitings);
+
+  var filteredData = sitings.filter(spot => spot.date === inputValue);
+  console.log(filteredData);
+
+  // BONUS: Calculate summary statistics for the age field of the filtered data
+
+  // First, create an array with just the age values
+  //var selected = filteredData.map(siting => siting.data);
+
+  // Next, use math.js to calculate the mean, median, mode, var, and std of the ages
+  //var mean = math.mean(ages);
+  //var median = math.median(ages);
+  //var mode = math.mode(ages);
+  //var variance = math.var(ages);
+  //var standardDeviation = math.std(ages);
+
+  // Then, select the unordered list element by class name
+  //var list = d3.select(".summary");
+
+  // remove any children from the list to
+  //list.html("");
+
+  // append stats to the list
+  //list.append("li").text(`Mean: ${mean}`);
+  //list.append("li").text(`Median: ${median}`);
+  //list.append("li").text(`Mode: ${mode}`);
+  //list.append("li").text(`Variance: ${variance}`);
+  //list.append("li").text(`Standard Deviation: ${standardDeviation}`);
 });
+
