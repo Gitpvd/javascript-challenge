3acced86c3325747d2cd7ca784bca29746b12345
Gitpvd <pvdayanand@gmail.com>
2019-09-21 21:53:13 -0500

    update 7

--- UFO-Level 1/static/js/index.js
+++ UFO-Level 1/static/js/index.js
@@ -1,28 +1,54 @@
 
+var sitings= data;
+var button = d3.select("#myButton");
+var ipDate= d3.select("#myInput");
+
 // Grabbing a reference to the table body
 var tbody = d3.select("tbody");
 
 //Iterating through sitings data and displaying it using arrow functions
-data.forEach(sitingsReport => {
+sitings.forEach(siting => {
   var row = tbody.append("tr");
-  Object.entries(sitingsReport).forEach(([key, value]) => {
+  Object.entries(sitings).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
   });
 });
 
-var sitings= data;
-var filterDate = "00/00/0000";
-var button = d3.select("#myButton");
-var ipDate= d3.select("#myInput");
-
 button.on("click", function() {
-  console.log("Hi, a button was clicked!");
+  console.log("Hello! Please check console log for filtered data!");
+  //delete previous table contents except header row
+  //var table = document.getElementById("myTable");
+  //for(var i = table.rows.length - 1; i > 0; i--)
+  //{
+    //table.deleteRow(i);
+  //}
+  //get inputted date value
   var x = document.getElementById("myInput").value;
   document.getElementById("demo").innerHTML = x;
-  var filtered = sitings.filter(function (siting) {
-    return siting.datetime === x;
+  //filter sitings data by inputted date
+  var filtered = sitings.filter(function (siting) 
+  {
+    return siting.datetime === x
   });
+  console.log(filtered);
+  var tbody = d3.select("tbody")
+  filtered.forEach(event=> {
+    var row = tbody.append("tr");
+    Object.entries(event).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value); 
+    });
+    alert("Please check console log for filtered data!")
+  });
+   //create new table body with filtered data 
+  //var tbody = d3.select("tbody")
+  //filtered.forEach(event=> {
+    //var rowf = tbody.append("tr");
+    //Object.entries(event).forEach(([key, value]) => {
+      //var cellf = rowf.append("td");
+      //cellf.text(value);
+    //});
 });
 
 
